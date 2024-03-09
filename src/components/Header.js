54cb07a418ee8974587c0ca9d6a99b6f9baa8edb
Ambika Prasad <akashboard2021@gmail.com>
2024-03-09 10:51:11 +0530

    added file upload and generate data insights functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,31 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <div className={styles[`header-section`]}>
       <header className={styles.header}>
-        <div className={styles["logo-container"]}>
-          <img src="images/Logo.jpg" alt="Logo" className={styles.logo} />
-        </div>
+        <Link style={{ textDecoration: "none", cursor: "pointer" }} to={"/"}>
+          <div className={styles["logo-container"]}>
+            <img src="images/Logo.jpg" alt="Logo" className={styles.logo} />
+          </div>
+        </Link>
+
         <nav className={styles["nav-bar"]}>
           <ul>
             <li>Chemical Calculator</li>
-            <li>File Upload</li>
+            <li>
+              <Link
+                to={"/data-visulization"}
+                style={{
+                  cursor: "pointer",
+                  textDecoration: "none",
+                  color: "inherit",
+                }}
+              >
+                File Upload
+              </Link>{" "}
+            </li>
             <li>Chemical E-commerce</li>
           </ul>
         </nav>
