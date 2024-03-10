fec686ef9f8fdff980ef2aba946ec4a6c5fb490a
ABHI2ME <abhignoneclsingh@gmail.com>
2024-03-10 15:04:20 +0530

    no changes

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,27 @@
-// Header.js
+// // Header.js
 
-import React from 'react';
-import './Header.css'; // Import the CSS file
+// import React from 'react';
+// import './Header.css'; // Import the CSS file
 
-const Header = () => {
-  return (
-    <header className="header">
+// const Header = () => {
+//   return (
+//     <header className="header">
        
-      <div className="logo-container">
-        <img src="images/Logo.jpg" alt="Logo" className="logo" />
-      </div>
-      <nav className="nav-bar">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Services</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-    </header>
-  );
-};
-export default Header;
+//       <div className="logo-container">
+//         <img src="images/Logo.jpg" alt="Logo" className="logo" />
+//       </div>
+//       <nav className="nav-bar">
+//         <ul>
+//           <li>Home</li>
+//           <li>About</li>
+//           <li>Services</li>
+//           <li>Contact</li>
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// };
+// export default Header;
 
 
 
